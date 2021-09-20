64699b0b8789ac5353c9321a7a73eb7986662cb6
mardonio.costa <mardonio.costa@tecnisys.com.br>
2021-09-20 10:03:35 -0300

    Layout html5 em teste

--- app/Views/Tests/index.php
+++ app/Views/Tests/index.php
@@ -1,6 +1,13 @@
-<?php
-
-echo $titulo;
-echo '<br>';
-echo $subtitulo;
-echo '<br>';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1><?php echo $titulo ?></h1>
+  <h1><?php echo $subtitulo ?></h1>
+</body>
+</html>
