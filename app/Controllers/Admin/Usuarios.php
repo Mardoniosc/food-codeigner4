15895abb72b058356faad355254e8a7991a465bb
mardonio.costa <mardonio.costa@tecnisys.com.br>
2021-09-21 11:13:23 -0300

    listando usuario do banco de dados

--- app/Controllers/Admin/Usuarios.php
+++ app/Controllers/Admin/Usuarios.php
@@ -3,10 +3,21 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Models\UsuarioModel;
 
-class Usuarios extends BaseController
-{
+class Usuarios extends BaseController {
+    
+    private $usuarioModel;
+
+    public function __construct() {
+
+        $this->usuarioModel = new UsuarioModel();
+    }
+
     public function index() {
-        //
+        
+        $usuarios = $this->usuarioModel->findAll();
+        
+        dd($usuarios);
     }
 }
--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -9,14 +9,14 @@ class UsuarioModel extends Model
     protected $table                = 'usuarios';
     protected $returnType           = 'object';
     protected $useSoftDeletes       = true;
-    protected $allowedFields        = ['nome', 'email', 'telefone'];
+    protected $allowedFields        = ['nome', 'email', 'telefone', 'cpf'];
 
-    // // Dates
-    // protected $useTimestamps        = true;
-    // protected $dateFormat           = 'datetime';
-    // protected $createdField         = 'criado_em';
-    // protected $updatedField         = 'atualizado_em';
-    // protected $deletedField         = 'deletado_em';
+    // Dates
+    protected $useTimestamps        = true;
+    protected $dateFormat           = 'datetime';
+    protected $createdField         = 'criado_em';
+    protected $updatedField         = 'atualizado_em';
+    protected $deletedField         = 'deletado_em';
 
 
 }
