97f6276559ba0a0d94da97a43b7a94df6568d929
mardonio.costa <mardonio.costa@tecnisys.com.br>
2021-10-08 09:43:50 -0300

    feat: :sparkles: Criando a migração de medidas e listando as mesmas
    Feito a criação da migração adicionado menu no layout principal e criado index para listar as medidas e configrado o controller principal

---
+++ app/Models/MedidaModel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class MedidaModel extends Model
+{
+
+    protected $table                = 'medidas';
+    protected $returnType           = 'App\Entities\Medida';
+    protected $useSoftDeletes       = true;
+    protected $allowedFields        = ['nome', 'descricao', 'ativo'];
+
+    // Dates
+    protected $useTimestamps        = true;
+    protected $dateFormat           = 'datetime';
+    protected $createdField         = 'criado_em';
+    protected $updatedField         = 'atualizado_em';
+    protected $deletedField         = 'deletado_em';
+
+    // validações
+    protected $validationRules = [
+        'nome' => 'required|min_length[3]|max_length[120]|is_unique[medidas.nome]',
+    ];
+
+    protected $validationMessages = [
+        'nome' => [
+            'required' => 'Campo nome ainda não foi preenchido!',
+            'is_unique' => 'Desculpe. Essa medida já existe na base!',
+        ],
+    ];
+}
