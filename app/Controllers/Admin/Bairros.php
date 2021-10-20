c7d6ca4b4c10998ecf6e29f181f72bed122a1112
mardonio.costa <mardonio.costa@tecnisys.com.br>
2021-10-20 11:36:12 -0300

    feat: :sparkles: Listando os bairros da base de dados

--- app/Controllers/Admin/Bairros.php
+++ app/Controllers/Admin/Bairros.php
@@ -3,11 +3,26 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Models\BairroModel;
 
-class Bairros extends BaseController
-{
-    public function index()
-    {
-        //
+class Bairros extends BaseController {
+
+    private $bairroModel;
+
+    public function __construct() {
+        $this->bairroModel = new BairroModel();
+    }
+
+    public function index() {
+
+        $data = [
+            'titulo' => 'Listando os bairros atendidos',
+            'bairros' => $this->bairroModel->withDeleted(true)->paginate(10),
+            'pager' => $this->bairroModel->pager,
+        ];
+
+        
+        
+        return view('Admin/Bairros/index', $data);
     }
 }
