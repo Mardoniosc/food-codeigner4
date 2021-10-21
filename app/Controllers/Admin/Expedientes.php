11955bebe5b7d8663cc7ed5ffed6cc9cc4fb584f
mardonio.costa <mardonio.costa@tecnisys.com.br>
2021-10-21 11:48:40 -0300

    feat: :sparkles: Trabalhando na view expedientes

--- app/Controllers/Admin/Expedientes.php
+++ app/Controllers/Admin/Expedientes.php
@@ -3,11 +3,23 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Models\ExpedienteModel;
 
 class Expedientes extends BaseController
 {
-    public function index()
-    {
-        //
+    private $expedienteModel;
+
+    public function __construct() {
+        $this->expedienteModel = new ExpedienteModel();
+    }
+
+    public function expedientes() {
+
+        $data = [
+            'titulo' => 'Gerenciar o horário de funcionamento',
+            'expedientes' => $this->expedienteModel->findAll(),
+        ];
+
+        return view('Admin/Expedientes/expedientes', $data);
     }
 }
